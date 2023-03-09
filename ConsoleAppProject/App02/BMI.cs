007aadb94a38d7d33512406c2bc9ce2451ecc231
VzGriffin <Mylesleyton12@gmail.com>
2023-03-09 22:25:03 +0000

    Compleated the BMI calculator, have added screreenshots and given the choice to sellect between BMI and Distance converter.

--- ConsoleAppProject/App02/BMI.cs
+++ ConsoleAppProject/App02/BMI.cs
@@ -1,4 +1,7 @@
-﻿namespace ConsoleAppProject.App02
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace ConsoleAppProject.App02
 {
     /// <summary>
     /// Please describe the main features of this App
@@ -8,5 +11,58 @@
     /// </author>
     public class BMI
     {
+        public void CalculateIndex()
+        {
+            Console.WriteLine("\n================================================");
+            Console.WriteLine("                    BMi calculator                ");
+            Console.WriteLine("                   by Myles Griffiths             ");
+            Console.WriteLine(" =================================================\n");
+
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Enter your Waght (kg): ");
+            double kg = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter your height (m) ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            double BMI = kg / (height * height);
+            Console.WriteLine("Your BMI is:" + Math.Round(BMI, 2));
+            Console.ReadKey();
+
+            if (BMI < 16)
+                Console.WriteLine("You are Severly Underweight");
+            else
+                if (BMI <= 18.5)
+                Console.WriteLine("Your underweight");
+            else
+                if (BMI <= 25)
+                Console.WriteLine("You have normal Weight");
+            else
+                if (BMI <= 30)
+                Console.WriteLine("You're Overweight");
+            else
+                if (BMI <= 35)
+                Console.WriteLine("You're moderately obese");
+            else
+                Console.WriteLine("Your severely obese");
+
+        }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ConsoleAppProject.App01;
+using ConsoleAppProject.App02;
 using ConsoleAppProject.App03;
 using ConsoleAppProject.Helpers;
 using System;
@@ -15,6 +16,9 @@ namespace ConsoleAppProject
     /// </summary>
     public static class Program
     {
+        private static DistanceConverter converter = new DistanceConverter();
+
+        private static BMI calculator = new BMI();
         public static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
@@ -22,12 +26,30 @@ namespace ConsoleAppProject
             Console.WriteLine();
             Console.WriteLine(" =================================================");
             Console.WriteLine("    BNU CO453 Applications Programming 2022-2023! ");
+            Console.WriteLine("                By Myles Griffiths                ");
             Console.WriteLine(" =================================================");
             Console.WriteLine();
 
-            DistanceConverter converter = new DistanceConverter();
+            Console.WriteLine("1. Distance Converter");
+            Console.WriteLine("2. BMI Calculator");
+            Console.WriteLine();
 
-            converter.ConvertDistacne(); 
+            Console.Write("Please enter your choice of app > ");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                converter.ConvertDistacne();
+            }
+            else if (choice == "2")
+            {
+                calculator.CalculateIndex();
+            }
+            else Console.WriteLine("Invalid Choice");
+
+            
+
+            
         }
     }
 }
